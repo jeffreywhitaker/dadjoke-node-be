4ee81b91f9da776e03779ea518b22abca70369f0
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2020-12-17 05:56:33 -0800

    change index.js from async to .then

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 import connectDB from "./config/dbConfig.js";
 import server from "./server.js";
 
-async function init() {
-  await connectDB();
-
+connectDB().then(() => {
   const port = process.env.PORT || 5000;
   server.listen(port, () => {
     console.log(`Joke server listening on: ${port}`);
   });
-}
-
-init();
+});
