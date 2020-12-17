0c5d5a7ac3f488923231ec6954dea8a46ad4afb8
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2020-12-17 09:56:49 -0800

    update cookie options

--- server.js
+++ server.js
@@ -46,16 +46,16 @@ server.use(bodyParser.json());
 server.use(
   session({
     secret: process.env.SECRET,
-    resave: true,
-    saveUninitialized: true,
+    // resave: true,
+    // saveUninitialized: true,
     store: new MongoStore({
       mongooseConnection: mongoose.connection,
       collection: "sessions",
     }),
     cookie: {
-      sameSite: "none",
-      httpOnly: false,
-      secure: true,
+      // sameSite: "none",
+      // httpOnly: false,
+      // secure: true,
       maxAge: 1000 * 60 * 60 * 24 * 3, // 3 days
     },
     rolling: true,
