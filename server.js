972317c7be86538d3a3559446c8bfbeb3a963e9a
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2020-12-17 10:16:20 -0800

    set secure true

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ server.use(
     cookie: {
       sameSite: "none",
       // httpOnly: false,
-      // secure: true,
+      secure: true,
       maxAge: 1000 * 60 * 60 * 24 * 3, // 3 days
     },
     rolling: true,
