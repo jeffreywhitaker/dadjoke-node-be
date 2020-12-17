e51009a5c599bb6ed5f7f92d08af1ece667be21c
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2020-12-17 12:30:50 -0800

    fix typo

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ import session from "express-session";
 import mongoose from "mongoose";
 import connectMongo from "connect-mongo";
 import passportConfig from "./config/passportConfig.js";
-import routesMD from "./ROUTES.md";
 
 import jokeRoutes from "./routes/jokeRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -74,7 +73,6 @@ server.use(passport.session());
 // routes
 server.use("/api/jokes/", jokeRoutes);
 server.use("/api/users/", userRoutes);
-server.get("/", express.static(routesMD));
 
 // export
 export default server;
