2d69a06185f4aa2cb47b0e6be3203858a013d1be
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2020-12-17 23:17:39 -0800

    same site none

--- server.js
+++ server.js
@@ -57,8 +57,7 @@ server.use(
       collection: "sessions",
     }),
     cookie: {
-      // sameSite: "none",
-      // httpOnly: false,
+      sameSite: "none",
       // secure: true,
       maxAge: 1000 * 60 * 60 * 24 * 3, // 3 days
     },
